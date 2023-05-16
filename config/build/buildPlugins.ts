f4448e4f671548800eb0d849fc168127c99a22d5
Regina P <pavlova5577@yandex.ru>
2023-05-16 12:52:10 +0300

    fix dev plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,19 +1,27 @@
 import HTMLWebpackPlugin from "html-webpack-plugin";
 import path from "path";
-import webpack, { DefinePlugin } from "webpack";
+import webpack, { DefinePlugin, ids } from "webpack";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 import { BuildOptions } from "./types/config";
 
-export function buildPlugins({ paths,
-  isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
+export function buildPlugins({
+  paths,
+  isDev,
+}: BuildOptions): webpack.WebpackPluginInstance[] {
+  const plugins = [
     new HTMLWebpackPlugin({ template: paths.html }),
     new webpack.ProgressPlugin(),
-    new MiniCssExtractPlugin({ filename: "css/[name].[contenthash:8].css",
-      chunkFilename: "css/[name].[contenthash:8].css" }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[contenthash:8].css",
+      chunkFilename: "css/[name].[contenthash:8].css",
+    }),
     new webpack.DefinePlugin({ __IS_DEV__: JSON.stringify(isDev) }),
-    new webpack.HotModuleReplacementPlugin(),
-    new BundleAnalyzerPlugin({ openAnalyzer: false }),
   ];
+
+  if (isDev) {
+    plugins.push(new webpack.HotModuleReplacementPlugin());
+    plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }));
+  }
+  return plugins;
 }
