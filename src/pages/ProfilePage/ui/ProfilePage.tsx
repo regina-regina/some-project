d420c36d9498201d40fcbc54fb09613b828c13e1
Regina Pavlova <pavlova5577@yandex.ru>
2023-05-31 19:38:58 +0300

    validate form

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -106,10 +106,11 @@ const ProfilePage = ({ className }: ProfilePageProps) => {
     <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
       <div className={classNames("", {}, [className])}>
         <ProfilePageHeader />
-        {validateErrors?.length &&
-          validateErrors.map((err) => (
+        {validateErrors?.length
+          ? validateErrors.map((err) => (
             <Text theme={TextTheme.ERROR} text={err} />
-          ))}
+          ))
+          : null}
         <ProfileCard
           data={formData}
           isLoading={isLoading}
